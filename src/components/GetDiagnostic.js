c65626b28b69f153fc6a720f1170923288fbb168
elfailali <el.failali.lbalghiti@gmail.com>
2024-01-07 21:25:26 +0100

    middle commit

--- src/components/GetDiagnostic.js
+++ src/components/GetDiagnostic.js
@@ -105,28 +105,29 @@ export default function GetDiagnostic() {
     };
     
   return (
-    <div className='bg-gradient-to-t from-lime-400 to-lime-50'>  
+    <>
+      <div className='bg-gradient-to-t from-lime-400 to-lime-50 flex flex-row py-20'>  
+        <div className=' rightComponent '>
+        <div className="items-center self-stretch flex flex-col justify-center px-16 py-4 max-md:max-w-full max-md:px-5">
+              <div className="flex w-[686px] max-w-full flex-col items-stretch">
+                <h2 className="max-w-[684px] text-zinc-900 text-start text-5xl font-semibold leading-[57px] max-md:max-w-full">
+                  Plant Disease Detection
+                </h2>
+                <h4 className="justify-start self-start mt-4 text-zinc-800 text-start text-lg leading-7 max-w-[582px] max-md:max-w-full">
+                  We cannot allow your plants to suffer from disorders and various diseases damages. Let’s begin identifies your plant diseases and improve productivity and get
+                  farming knowledge for free.
+                </h4>
+              </div>
+        </div>
 
-      <div className="items-center self-stretch flex flex-col justify-center px-16 py-4 max-md:max-w-full max-md:px-5">
-            <div className="flex w-[686px] max-w-full flex-col items-stretch">
-              <h2 className="max-w-[684px] text-zinc-900 text-center text-5xl font-semibold leading-[57px] max-md:max-w-full">
-                Plant Disease Detection
-              </h2>
-              <h4 className="justify-center self-center text-zinc-800 text-center text-lg leading-7 max-w-[582px] max-md:max-w-full">
-                identifies your plant diseases and improve productivity and get
-                farming knowledge for free.
-              </h4>
-            </div>
-      </div>
-
-      <div className='flex justify-center '>
+      {/* <div className='flex justify-center '>
         <img  src='https://petapixel.com/assets/uploads/2019/06/identifynaturefeattt.jpg' className="h-full me-2 rounded-lg shadow-sm overflow-hidden self-center max-w-full"  width={600} height={500}></img>
-      </div>
-      <h3 className='text-green text-5xl font-semibold mx-auto text-center mt-6 '>
-        Get a Diagnostic
-      </h3>
-      <div className="flex items-center justify-center  mx-20 ">
-          <div className='py-2 px-20  '>
+      </div> */}
+
+
+
+      <div className="flex items-start justify-start  mx-16 ">
+          {/* <div className='py-2 px-20  '>
               <button onClick={handleTakePicture}  className=" bg-blue-700 text-white font-bold rounded hover:bg-blue-800  py-2 px-4 mt-2">
 
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-6 mx-auto">
@@ -135,40 +136,55 @@ export default function GetDiagnostic() {
                 </svg>
                 Take a picture
               </button>
-          </div>
+          </div> */}
 
 
+
+          {!isLoading &&   
+            <div className="py-2 px-20 items-start flex justify-between gap-2 max-md:ml-1.5 max-md:mr-1.5">
+
+                    <button className="bg-blue-700 text-white shadow-lg font-bold rounded-full hover:bg-blue-800 py-2 px-8 mt-2 flex flex-row" onClick={handleUploadButtonClick}>
+                      <img
+                        loading="lazy"
+                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/908909157dc6d06cad801de6f07c9f921aaa611ce109d370afd628473cd40422?apiKey=49a8e08e74bb44d9affdaac81f6699af&"
+                        className="aspect-[1.04] object-contain object-center w-[25px] overflow-hidden shrink-0 max-w-full my-auto"
+                      />
+                      <div className="text-white text-center text-lg font-semibold leading-7 self-stretch grow whitespace-nowrap px-2">
+                        Diagnose a plant
+                      </div>
+                    </button>
+                    <input
+                      className='bg-white hidden'
+                      ref={fileInputRef}
+                      type="file"
+                      accept="image/*"
+                      onChange={handleImageChange}
+                    />
+            </div>
+          } 
           {isLoading && 
-            <CardContent className='flex flex-col items-center 	'>
-                <CircularProgress  className='bg-midnight'/>
-                <Typography variant="h6" noWrap>
-                  Processing
-                </Typography>
-            </CardContent>
-          }
-
-
-
-          <div className="py-2 px-20 ">
-
-                  <button className="bg-blue-700 text-white font-bold rounded hover:bg-blue-800 py-2 px-4 mt-2" onClick={handleUploadButtonClick}>
-
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-6 mx-auto">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"/>
-                    </svg>
-                    Upload a picture
-                  </button>
-                  <input
-                    className='bg-white hidden'
-                    ref={fileInputRef}
-                    type="file"
-                    accept="image/*"
-                    onChange={handleImageChange}
-                  />
-          </div>
+                  <CardContent className='flex flex-col items-center w-16	justify-center ml-16 items-center'>
+                      <CircularProgress  className='bg-midnight'/>
+                      <Typography variant="h6" noWrap>
+                        Processing
+                      </Typography>
+                  </CardContent>
+                }
       </div>
 
-      
+
+      </div>
+
+      <div className=" left flex flex-col items-stretch w-6/12  max-md:w-full max-md:ml-0">
+            <img
+              loading="lazy"
+              srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/5b0e2edb2e7cf0c0ad339c62113cc3eb17f3341b60166d4b772520720f6d8cf2?apiKey=49a8e08e74bb44d9affdaac81f6699af&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/5b0e2edb2e7cf0c0ad339c62113cc3eb17f3341b60166d4b772520720f6d8cf2?apiKey=49a8e08e74bb44d9affdaac81f6699af&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/5b0e2edb2e7cf0c0ad339c62113cc3eb17f3341b60166d4b772520720f6d8cf2?apiKey=49a8e08e74bb44d9affdaac81f6699af&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/5b0e2edb2e7cf0c0ad339c62113cc3eb17f3341b60166d4b772520720f6d8cf2?apiKey=49a8e08e74bb44d9affdaac81f6699af&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/5b0e2edb2e7cf0c0ad339c62113cc3eb17f3341b60166d4b772520720f6d8cf2?apiKey=49a8e08e74bb44d9affdaac81f6699af&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/5b0e2edb2e7cf0c0ad339c62113cc3eb17f3341b60166d4b772520720f6d8cf2?apiKey=49a8e08e74bb44d9affdaac81f6699af&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/5b0e2edb2e7cf0c0ad339c62113cc3eb17f3341b60166d4b772520720f6d8cf2?apiKey=49a8e08e74bb44d9affdaac81f6699af&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/5b0e2edb2e7cf0c0ad339c62113cc3eb17f3341b60166d4b772520720f6d8cf2?apiKey=49a8e08e74bb44d9affdaac81f6699af&"
+              className="aspect-[2.83] object-contain object-center w-full max-w-[1320px] overflow-hidden grow  max-md:max-w-full max-md:mt-10"
+            />
+      </div>
+
     </div>
+    </>
+    
   )
 }
